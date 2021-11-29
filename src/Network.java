6c4e8a0794ac508acaa1fd11c6a37eb02318f82e
DrInnovatio <turing1723@gmail.com>
2021-11-29 16:18:22 +0900

    Started learning Java Network.

--- src/Network.java
+++ src/Network.java
@@ -5,9 +5,38 @@ public class Network  {
 
     public static void main(String[] args) throws Exception {
 
-        InetAddress ia = InetAddress.getByName("www.naver.com");
+        InetAddress ia = null;
+        InetAddress ia1 = null;
+        InetAddress[] ia2 = null;
 
+        try{
+            ia = InetAddress.getLocalHost();
+            ia1 = InetAddress.getByName("www.naver.com");
+            ia2 = InetAddress.getAllByName("www.google.com");
+        } catch (UnknownHostException ignored){}
 
-        System.out.println(ia.getHostAddress());
+        System.out.println("ia.hostAddress = " + ia.getHostAddress());
+        System.out.println("ia.hostName = " + ia.getHostName());
+        System.out.println();
+        System.out.println("ia1.hostAddress = " + ia1.getHostAddress());
+        System.out.println("ia1.hostName = " + ia1.getHostName());
+        System.out.println();
+
+        for(int i = 0; i < ia2.length; i++){
+            System.out.println("ia2[" + i + "].hostAddress = " + ia2[i].getHostAddress());
+            System.out.println("ia2[" + i + "].hostName = " + ia2[i].getHostName());
+        }
     }
 }
+
+/**
+ * 2. TCP 통신
+ *
+ * TPC 통신의 장점과 단점
+ *
+ * TPC 구현을 위한 클래스들.
+ *- InetAddress class 어디서든 사용가능.
+ * -Socket class
+ * -ServerSocket class
+ *
+ * **/
---
+++ src/SocketNet.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+
+public class SocketNet {
+
+    public static void main(String[] args) throws IOException {
+        Socket soc = null;
+        InetAddress ia = null;
+
+        try {
+            ia = InetAddress.getByName("www.daum.net");
+        }catch(UnknownHostException e){}
+
+        try {
+            soc = new Socket(ia, 80);
+            String request = "GET http://www.daum.net/index.html HPPT1/0\r\n\n";
+            //Output
+            OutputStreamWriter osw = new OutputStreamWriter(soc.getOutputStream());
+            BufferedWriter bos = new BufferedWriter(osw, 512);
+            PrintWriter pw = new PrintWriter(bos);
+            pw.println(request);
+            pw.flush();
+            //input
+            InputStreamReader isr = new InputStreamReader(soc.getInputStream());
+            BufferedReader br = new BufferedReader(isr, 512);
+
+            while(true){
+                String str = br.readLine();
+                if(str == null)
+                    break;
+                System.out.println(str);
+            }
+
+            pw.close();
+            br.close();
+
+        } catch (IOException e){}
+    }// main
+}//class
+
+//must memory this page.
+//must memory this page.
+//must memory this page.
+//must memory this page.
+//must memory this page.
